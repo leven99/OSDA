b95f3cd80086a309139fad04d85460dac21702fa
Leven <levenchn@163.com>
2019-07-15 08:06:58 +0800

    feat: 完善MVVM模型
    Signed-off-by: Leven <levenchn@163.com>

---
+++ Models/Timer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace OSerialPort.Models
+{
+    class Timer
+    {
+        ViewModels.MainWindow VMmainWindow = new ViewModels.MainWindow();
+
+        /// <summary>
+        /// 定时器注册
+        /// </summary>
+        public void InitClockTimer()
+        {
+            DispatcherTimer DTDispatcherTimer = new DispatcherTimer
+            {
+                Interval = new TimeSpan(0, 0, 1),
+                IsEnabled = true
+            };
+            DTDispatcherTimer.Tick += ClockTimer_Tick;
+            DTDispatcherTimer.Start();
+        }
+
+        public void ClockTimer_Tick(object sender, EventArgs e)
+        {
+            VMmainWindow.SystemTimeData();
+        }
+    }
+}
---
+++ Views/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows;
+
+namespace OSerialPort
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ViewModels.MainWindow VMmainWindow = new ViewModels.MainWindow();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        #region 打开/关闭串口
+        private void OpenCloseSP(string empty)
+        {
+            VMmainWindow.OpenSP();
+        }
+        #endregion
+
+        #region 发送
+        private void Send(string empty)
+        {
+            VMmainWindow.Send();
+        }
+        #endregion
+
+        #region 多项发送
+        #endregion
+
+        #region 路径选择
+        #endregion
+
+        #region 清接收区
+        private void ClarReceData(string empty)
+        {
+            VMmainWindow.ClarReceData();
+        }
+        #endregion
+
+        #region 清发送区
+        private void ClearSendData(string empty)
+        {
+            VMmainWindow.ClearSendData();
+        }
+        #endregion
+
+        #region 清空计数
+        private void ClearCount(string empty)
+        {
+            VMmainWindow.ClearCount();
+        }
+        #endregion
+    }
+}
