55ff19973f235de2813551bb3ea75390320d1e35
Leven <levenchn@163.com>
2019-07-18 15:12:17 +0800

    fix: 修复串口打开失败的问题

--- ViewModels/MainWindowVM.cs
+++ ViewModels/MainWindowVM.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.IO.Ports;
+using System.Windows.Media;
 using System.Windows.Threading;
 
 namespace OSerialPort.ViewModels
@@ -10,92 +11,117 @@ namespace OSerialPort.ViewModels
     {
         private SerialPort SPserialPort = null;
 
-        /// <summary>
-        /// 串口配置
-        /// </summary>
-        public string[] SPPort { get; set; }
-        public int[] SPBaudRate { get; set; }
-        public int[] SPDataBits { get; set; }
-        public List<StopBits> SPStopbit { get; set; }
-        public List<Parity> SPParity { get; set; }
-
-        /// <summary>
-        /// 接收区数据
-        /// </summary>
-        public string ReceData { get; set; }
-
-        public void ClarReceData()
-        {
-            ReceData = string.Empty;
-
-        }
-
-        /// <summary>
-        /// 发送区数据
-        /// </summary>
-        public string SendData { get; set; }
-
-        public void ClearSendData()
-        {
-            SendData = string.Empty;
-        }
-
-        /// <summary>
-        /// 接收区
-        /// </summary>
-        public string ReceHeadrt { get; set; }
-
-        public UInt32 ReceDataCount { get; set; }
-
-        public string ReceAutoSave { get; set; }
-
-        /// <summary>
-        /// 发送区
-        /// </summary>
-        public string SendHeader { get; set; }
-
-        public UInt32 SendDataCount { get; set; }
-
-        /// <summary>
-        /// 清空计数（接收区字节计数和发送区字节计数同时清空）
-        /// </summary>
-        public void ClearCount()
-        {
-            ReceDataCount = 0;
-            SendDataCount = 0;
-        }
-
-        #region 停止位和校验位计算
-        public static StopBits GetStopBits(string emp)
-        {
-            StopBits bits = StopBits.None;
-            switch (emp)
-            {
-                case "One": bits = StopBits.One; break;
-                case "Two": bits = StopBits.Two; break;
-                case "OnePointFive": bits = StopBits.OnePointFive; break;
-            }
-            return bits;
-        }
-
-        public static Parity GetParity(string emp)
-        {
-            Parity parity = Parity.None;
-            switch (emp)
-            {
-                case "None": parity = Parity.None; break;
-                case "Even": parity = Parity.Even; break;
-                case "Mark": parity = Parity.Mark; break;
-                case "Odd": parity = Parity.Odd; break;
-                case "Space": parity = Parity.Space; break;
-            }
-            return parity;
-        }
+        #region 串口集
+        public string[] LSPPort { get; set; }
+        public int[] LSPBaudRate { get; set; }
+        public int[] LSPDataBits { get; set; }
+        public string[] LSPStopbits { get; set; }
+        public string[] LSPParity { get; set; }
         #endregion
 
-        /// <summary>
-        /// 打开串口/发送串口
-        /// </summary>
+        #region 串口配置
+        public string _SPPort;
+        public string SPPort
+        {
+            get
+            {
+                return _SPPort;
+            }
+            set
+            {
+                if(_SPPort != value)
+                {
+                    _SPPort = value;
+                    RaisePropertyChanged("SPPort");
+                }
+            }
+        }
+
+        public int _SPBaudRate;
+        public int SPBaudRate
+        {
+            get
+            {
+                return _SPBaudRate;
+            }
+            set
+            {
+                if(_SPBaudRate != value)
+                {
+                    _SPBaudRate = value;
+                    RaisePropertyChanged("SPBaudRate");
+                }
+            }
+        }
+
+        public int _SPDataBits;
+        public int SPDataBits
+        {
+            get
+            {
+                return _SPDataBits;
+            }
+            set
+            {
+                if(_SPDataBits != value)
+                {
+                    _SPDataBits = value;
+                    RaisePropertyChanged("SPDataBits");
+                }
+            }
+        }
+
+        public StopBits _SPStopbits;
+        public StopBits SPStopbits
+        {
+            get
+            {
+                return _SPStopbits;
+            }
+            set
+            {
+                if(_SPStopbits != value)
+                {
+                    _SPStopbits = value;
+                    RaisePropertyChanged("SPStopbit");
+                }
+            }
+        }
+
+        public Parity _SPParity;
+        public Parity SPParity
+        {
+            get
+            {
+                return _SPParity;
+            }
+            set
+            {
+                if(_SPParity != value)
+                {
+                    _SPParity = value;
+                    RaisePropertyChanged("SPParity");
+                }
+            }
+        }
+
+        public Brush _SPBrush;
+        public Brush SPBrush
+        {
+            get
+            {
+                return _SPBrush;
+            }
+            set
+            {
+                if(_SPBrush != value)
+                {
+                    _SPBrush = value;
+                    RaisePropertyChanged("SPBrush");
+                }
+            }
+        }
+
         public string _OpenCloseSP;
         public string OpenCloseSP
         {
@@ -105,13 +131,125 @@ namespace OSerialPort.ViewModels
             }
             set
             {
-                if(_OpenCloseSP != value)
+                if (_OpenCloseSP != value)
                 {
                     _OpenCloseSP = value;
                     RaisePropertyChanged("OpenCloseSP");
                 }
             }
         }
+        #endregion
+
+        #region 接收区
+        public string ReceHeadrt { get; set; }
+
+        public UInt32 ReceDataCount { get; set; }
+
+        public string ReceAutoSave { get; set; }
+
+        public string ReceData { get; set; }
+        #endregion
+
+        #region 发送区
+        public string SendHeader { get; set; }
+
+        public UInt32 SendDataCount { get; set; }
+
+        public string SendData { get; set; }
+        #endregion
+
+        #region 辅助
+        public bool HexRece { get; set; }
+
+        public bool HexSend { get; set; }
+
+        public bool AutoSend { get; set; }
+
+        public UInt32 _AutoSendNum;
+        public UInt32 AutoSendNum
+        {
+            get
+            {
+                return _AutoSendNum;
+            }
+            set
+            {
+                if (_AutoSendNum != value)
+                {
+                    _AutoSendNum = value;
+                    RaisePropertyChanged("AutoSendNum");
+                }
+            }
+        }
+
+        public bool SaveRece { get; set; }
+
+        /// <summary>
+        /// 路径选择 - 用于修改接收接收时保存数据的路径
+        /// </summary>
+        public void SaveRecePath()
+        {
+            if (SaveRece)
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+        #endregion
+
+        #region 清空
+        public void ClarReceData()
+        {
+            ReceData = string.Empty;
+
+        }
+
+        public void ClearSendData()
+        {
+            SendData = string.Empty;
+        }
+
+        public void ClearCount()
+        {
+            ReceDataCount = 0;
+            SendDataCount = 0;
+        }
+        #endregion
+
+        #region 打开/关闭串口
+
+        #region 处理停止位和校验位
+        public StopBits GetStopBits(string emp)
+        {
+            StopBits stopBits = StopBits.One;
+            switch (emp)
+            {
+                case "One": stopBits = StopBits.One; break;
+                case "Two": stopBits = StopBits.Two; break;
+                case "OnePointFive": stopBits = StopBits.OnePointFive; break;
+                default: break;
+            }
+            return stopBits;
+        }
+
+        public Parity GetParity(string emp)
+        {
+            Parity parity = Parity.None;
+            switch (emp)
+            {
+                case "None": parity = Parity.None; break;
+                case "Even": parity = Parity.Even; break;
+                case "Mark": parity = Parity.Mark; break;
+                case "Odd": parity = Parity.Odd; break;
+                case "Space": parity = Parity.Space; break;
+                default: break;
+            }
+            return parity;
+        }
+        #endregion
 
         public bool OpenSP()
         {
@@ -124,11 +262,11 @@ namespace OSerialPort.ViewModels
             {
                 SPserialPort = new SerialPort
                 {
-                    PortName = SPPort.ToString(),
-                    BaudRate = int.Parse(SPBaudRate.ToString()),
-                    DataBits = int.Parse(SPDataBits.ToString()),
-                    StopBits = GetStopBits(SPStopbit.ToString()),
-                    Parity   = GetParity(SPParity.ToString())
+                    PortName = SPPort,
+                    BaudRate = SPBaudRate,
+                    DataBits = SPDataBits,
+                    StopBits = GetStopBits(SPStopbits.ToString()),
+                    Parity = GetParity(SPParity.ToString())
                 };
 
                 SPserialPort.Open();
@@ -137,6 +275,7 @@ namespace OSerialPort.ViewModels
 
                 if (SPserialPort.IsOpen)
                 {
+                    SPBrush = Brushes.GreenYellow;
                     OpenCloseSP = "关闭串口";
                     DepictInfo = string.Format("成功打开串行端口{0}、波特率{1}、数据位{2}、停止位{3}、校验位{4}", SPserialPort.PortName,
                     SPserialPort.BaudRate.ToString(), SPserialPort.DataBits.ToString(),
@@ -167,6 +306,7 @@ namespace OSerialPort.ViewModels
                 {
                     SPserialPort.Close();
 
+                    SPBrush = Brushes.Red;
                     OpenCloseSP = "打开串口";
                     DepictInfo = "串行端口关闭成功";
 
@@ -186,39 +326,9 @@ namespace OSerialPort.ViewModels
                 return false;
             }
         }
+        #endregion
 
-        /// <summary>
-        /// 16进制接收/发送
-        /// </summary>
-        public bool HexRece { get; set; }
-
-        public bool HexSend { get; set; }
-
-        /// <summary>
-        /// 自动发送
-        /// </summary>
-        public bool AutoSend { get; set; }
-
-        public UInt32 _AutoSendNum;
-        public UInt32 AutoSendNum
-        {
-            get
-            {
-                return _AutoSendNum;
-            }
-            set
-            {
-                if(_AutoSendNum != value)
-                {
-                    _AutoSendNum = value;
-                    RaisePropertyChanged("AutoSendNum.ToString()");
-                }
-            }
-        }
-
-        /// <summary>
-        /// 发送
-        /// </summary>
+        #region 发送/多项发送
         public void Send()
         {
             if (SPserialPort != null && SPserialPort.IsOpen)
@@ -248,27 +358,9 @@ namespace OSerialPort.ViewModels
                 }
             }
         }
+        #endregion
 
-        /// <summary>
-        /// 保存接收（默认保存在安装目录下以日期命名的.txt文本文件）
-        /// </summary>
-        public bool SaveRece { get; set; }
-
-        public void SaveRecePath()
-        {
-            if (SaveRece)
-            {
-
-            }
-            else
-            {
-
-            }
-        }
-
-        /// <summary>
-        /// 描述信息
-        /// </summary>
+        #region 状态栏
         string _DepictInfo;
         public string DepictInfo
         {
@@ -304,9 +396,6 @@ namespace OSerialPort.ViewModels
         }
         #endregion
 
-        /// <summary>
-        /// 系统时间
-        /// </summary>
         string _SystemTime;
         public string SystemTime
         {
@@ -324,6 +413,9 @@ namespace OSerialPort.ViewModels
             }
         }
 
+        /// <summary>
+        /// 获取系统当前时间和日期信息
+        /// </summary>
         public void SystemTimeData()
         {
             DateTime systemTime = DateTime.Now;
@@ -336,22 +428,21 @@ namespace OSerialPort.ViewModels
                 systemTime.Minute.ToString("00"),
                 systemTime.Second.ToString("00"));
         }
+        #endregion
 
-        /// <summary>
-        /// UI初始化
-        /// </summary>
         public MainWindowVM()
         {
-            SPPort = SerialPort.GetPortNames();
-            SPBaudRate = new int[] { 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200 };
-            SPDataBits = new int[] { 5, 6, 7, 8 };
-            SPStopbit = new List<StopBits> { StopBits.One, StopBits.OnePointFive, StopBits.Two };
-            SPParity = new List<Parity> { Parity.None, Parity.Even, Parity.Odd };
+            LSPPort = SerialPort.GetPortNames();
+            LSPBaudRate = new int[] { 1200, 2400, 4800, 9600, 14400, 19200, 38400, 57600, 115200 };
+            LSPDataBits = new int[] { 5, 6, 7, 8 };
+            LSPStopbits = new string[] { "One", "Two", "OnePointFive" };
+            LSPParity = new string[] { "None", "Even", "Odd", "Mark", "Space" };
 
+            SPBrush = Brushes.Red;
             OpenCloseSP = "打开串口";
 
             ReceDataCount = 0;
-            ReceAutoSave = "已停止";
+            ReceAutoSave  = "已停止";
             SendDataCount = 0;
 
             ReceHeadrt = "接收区：已接收" + ReceDataCount + "字节，接收自动保存[" + ReceAutoSave + "]";
